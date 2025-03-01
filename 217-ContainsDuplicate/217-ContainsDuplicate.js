873c7d66179ddaef009eff74b8d91b61edc3b37f
Shreya Bakshi <i.bakshi.shreya@gmail.com>
2025-03-01 16:58:39 +0100

    Create file

---
+++ 217-ContainsDuplicate/217-ContainsDuplicate.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    const map = new Map()
+    for(const n of nums){
+        if (map.has(n)) return true
+        map.set(n, true)
+    }
+    return false
+};
