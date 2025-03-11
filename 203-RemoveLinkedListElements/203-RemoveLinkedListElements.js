9e196e6b997163cb11400e702cc4275b53e8bdc5
Shreya Bakshi <i.bakshi.shreya@gmail.com>
2025-03-11 14:18:52 +0000

    Create file

---
+++ 203-RemoveLinkedListElements/203-RemoveLinkedListElements.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var removeElements = function(head, val) {
+
+    let dummy = new ListNode(-1)
+dummy.next = head
+    let prev = dummy
+    let curr = head
+
+    while (curr){
+        if (curr.val === val){
+            let nxt = curr.next
+            prev.next = nxt
+            curr = nxt
+        }
+        else{
+            let nxt = curr.next
+            prev = curr
+            curr = nxt
+        }
+    }
+    return dummy.next;
+};
