6ee82d1e5ab1d7a119044b909fdd39048c022fcd
Shreya Bakshi <i.bakshi.shreya@gmail.com>
2025-06-02 20:54:15 +0100

    File updated at 6/2/2025, 8:54:15 PM

--- 9-PalindromeNumber/9-PalindromeNumber.js
+++ 9-PalindromeNumber/9-PalindromeNumber.js
@@ -1,18 +1,24 @@
-// Last updated: 6/2/2025, 8:51:05 PM
+// Last updated: 6/2/2025, 8:54:15 PM
 /**
  * @param {number} x
  * @return {boolean}
  */
+// var isPalindrome = function(x) {
+//     if(x<0) return false;
+//     let arr = x.toString();
+//     left = 0;
+//     right = arr.length-1;
+
+//     while(left<right){
+//         if(arr[left] !== arr[right]) return false;
+//         left++;
+//         right--;
+//     }
+//     return true; 
+// };
 var isPalindrome = function(x) {
     if(x<0) return false;
-    let arr = x.toString();
-    left = 0;
-    right = arr.length-1;
-
-    while(left<right){
-        if(arr[left] !== arr[right]) return false;
-        left++;
-        right--;
-    }
-    return true; 
+    if (x === +x.toString().split("").reverse().join("")){
+        return true;
+    }else return false;
 };
