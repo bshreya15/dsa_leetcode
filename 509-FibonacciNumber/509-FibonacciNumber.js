5d938b2a03ec9ee829f94789f2a4254ff85c5829
Shreya Bakshi <i.bakshi.shreya@gmail.com>
2025-06-02 20:57:35 +0100

    Create 509-FibonacciNumber

---
+++ 509-FibonacciNumber/509-FibonacciNumber.js
@@ -0,0 +1,12 @@
+// Last updated: 6/2/2025, 8:57:34 PM
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function(n) {
+    if (n===0 || n===1) return n;
+
+    if (n>1){
+        return fib(n-1) + fib(n-2);
+    }
+};
