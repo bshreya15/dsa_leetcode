31e68c3b64912e877cab1f7a4ff803bc3612d050
Shreya Bakshi <i.bakshi.shreya@gmail.com>
2025-06-07 12:16:45 +0100

    File updated at 6/7/2025, 12:16:44 PM

--- 1-TwoSum/1-TwoSum.js
+++ 1-TwoSum/1-TwoSum.js
@@ -1,19 +1,30 @@
-// Last updated: 6/7/2025, 12:07:49 PM
+// Last updated: 6/7/2025, 12:16:44 PM
 /**
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-   let resArr = [];
+    // let resArr = [];
 
-    for(let left = 0 ; left <nums.length ; left++){
-        for(let right = 1 ; right <  nums.length ; right++){
-            if(nums[left]+nums[right] === target && left!=right){
-                resArr.push(left);
-                resArr.push(right);
-                return resArr;
-            }
+    // for(let left = 0 ; left <nums.length ; left++){
+    //     for(let right = 1 ; right <  nums.length ; right++){
+    //         if(nums[left]+nums[right] === target && left!=right){
+    //             resArr.push(left);
+    //             resArr.push(right);
+    //             return resArr;
+    //         }
+    //     }
+    // }
+
+    var obj = {};
+    for(let i=0; i<nums.length ; i++){
+        var num = nums[i];
+        if(obj[target-num] >= 0){
+            return [obj[target-num],i]
+        }
+        else{
+            obj[num] = i;
         }
     }
 };
